d0b258c82d6a57188b118440a7f895c83d5bfce3
taniot <gaetano.frascolla@gmail.com>
2023-03-27 12:10:10 +0200

    script: bootstrap

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,16 @@
 <script>
-    export default {
-        name: 'Header'
-    }
+export default {
+    name: 'Header'
+}
 </script>
 
 <template>
-    <header>
-        <h1>Rick and Morty</h1>
+    <header class="my-5">
+        <h1 class="text-center">Rick and Morty</h1>
+       
     </header>
 </template>
 
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './assets/scss/main.scss';
+
+//bootstrap
+// import * as bootstrap from 'bootstrap'
 import "@fontsource/montserrat"; // Defaults to weight 400.
 import App from './App.vue'
 
