d6ffbf57b2128c9c7d4db710fc3419c9190bcdeb
taniot <gaetano.frascolla@gmail.com>
2023-03-28 09:14:04 +0200

    refactor: app

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,26 @@ import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
-
-
 export default {
-  name:'App',
+  name: 'App',
   components: {
     AppHeader,
     AppMain,
     AppFooter
   },
-  data(){
+  data() {
     return {
       store
     }
   },
-  created(){
-            axios.get('https://rickandmortyapi.com/api/character')
-                .then((response) => {
-                    console.log(response);
-                    this.store.characters = response.data.results;
-                    this.store.charactersFound = response.data.results.length;
-                })
-        }
+  created() {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then((response) => {
+        console.log(response);
+        this.store.characters = response.data.results;
+        this.store.charactersFound = response.data.results.length;
+      })
+  }
 }
 </script>
 
